d8580c51337832e4e4fa97e17394bbc15fe9f2c6
shahinhaque <md.shahinhaque@gmail.com>
2021-09-13 22:22:55 +0600

    1st commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryModel.Repository;
+
+namespace RepositoryPattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private IStudentRepository studentRepository;
+
+        public StudentController(IStudentRepository studentRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
+
+ 
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RepositoryModel;
+using RepositoryModel.Helper;
 
 namespace RepositoryPattern
 {
@@ -25,6 +28,10 @@ namespace RepositoryPattern
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContextPool<ApplicationContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
